981df9661c8c0e1502c779b0da992092f62e0567
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-11-14 01:16:40 -0800

    Compile Sass files with Gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,30 +1,73 @@
 // Code pieced together from
+
 // https://css-tricks.com/gulp-for-beginners/
 // https://scotch.io/tutorials/how-to-use-browsersync-for-faster-development
 // https://travismaynard.com/writing/getting-started-with-gulp
-// https://github.com/gulpjs/gulp
-// https://browsersync.io/docs/gulp
-// https://github.com/gulpjs/gulp/blob/master/docs/recipes/minimal-browsersync-setup-with-gulp4.md
 
-// Include gulp
-// var gulp = require('gulp'); // pre-ES6
+// https://github.com/gulpjs/gulp
+// https://github.com/gulpjs/gulp/blob/master/docs/API.md
+// https://browsersync.io/docs/gulp
+
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/minimal-browsersync-setup-with-gulp4.md
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/server-with-livereload-and-css-injection.md
+
+// gulp.task([name,] fn)
+// https://gulpjs.org/API.html#gulp-task-name-fn
+
+/* 
+Build the source script file in src/scripts/, e.g. compiling with babel, minifying, etc.
+Put the compiled version in dist/scripts for use in index.html
+Watch for changes in the source file and rebuild the dist package
+With each rebuild of the dist package, reload the browser
+*/
+
+// Include gulp - Pre-ES6 method
+// var gulp = require('gulp');
+
+// Include gulp - ES6 method
 import gulp from "gulp";
 
-// Include plugin
-// var browserSync = require('browser-sync').create(); // create a browser sync instance.
-// const browserSync = require("browser-sync").create(); // also works
+// Include more modules
+import sass from "gulp-sass";
+import del from "del";
+
+// Include plugin - Pre-ES6 method
+// var browserSync = require('browser-sync').create(); // Create a browser sync instance.
+
+// Include plugin - ES6 method
 import browserSync from "browser-sync";
 
 const server = browserSync.create(); // Create a browser sync instance
 
+// Paths object
 const paths = {
   styles: {
-    src: "src/css/*.css"
+    src: "src/styles/*.scss",
+    dest: "dist/styles/"
   },
   scripts: {
-    src: "src/js/*.js"
+    src: "src/scripts/*.js",
+    dest: "dist/scripts/"
   }
 };
+
+// Wipe out the build directory, call before you build
+const clean = () => del(["dist"]);
+
+// Process styles
+function styles() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+// Process scripts
+function scripts() {
+  return gulp
+    .src(paths.scripts.src, { sourcemaps: true })
+    .pipe(gulp.dest(paths.scripts.dest));
+}
 
 // Development Tasks
 
@@ -42,7 +85,7 @@ const paths = {
 // });
 
 function reload(done) {
-  server.reload(); // const server = browserSync.create();
+  server.reload(); // server = browserSync.create();
   done();
 }
 
@@ -53,7 +96,7 @@ function reload(done) {
 function serve(done) {
   server.init({
     server: {
-      baseDir: "src"
+      baseDir: "./"
     }
   });
   done();
@@ -69,14 +112,15 @@ function serve(done) {
 // });
 
 const watch = () => {
-  gulp.watch("src/*.html", reload); // reload fxn defined earlier
-  gulp.watch(paths.styles.src, reload);
-  gulp.watch(paths.scripts.src, reload);
+  gulp.watch("*.html", reload); // reload fxn defined earlier. Callback fxn can also be gulp.series(reload) or gulp.parallel(reload)
+  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
 };
 
 // Default Task
 // gulp.task("default", ["watchFiles"]);
-const dev = gulp.series(serve, watch);
+const dev = gulp.series(clean, gulp.parallel(styles, scripts), serve, watch);
+
 export default dev;
 
 // `gulp` command execution order
